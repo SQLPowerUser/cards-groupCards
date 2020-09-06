7962d042e97374d35f450d4e4912783fbf730fec
SQLPowerUser <38347459+SQLPowerUser@users.noreply.github.com>
2020-09-06 11:43:11 +0300

    save1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cards-groupCards
 Карточки, группа карточек
+
+```
+git clone https://github.com/SQLPowerUser/cards-groupCards.git
+cd cards-groupCards
+npm i
+npm run dev
+```
---
+++ src/app/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="container-toolbar">
+            <p class="container-toolbar-legend">Фильтры</p>
+            <div class="container-toolbar-card-search">
+                Название карточки:&#160;
+                <input type="text" class="container-toolbar-search-text">
+            </div>
+            <div class="container-toolbar-card-state">
+                Статус:&#160;
+                <label class="mr12"> <input type="radio" name="card-state" checked> <span>любой</span> </label>
+                <label class="mr12"> <input type="radio" name="card-state"> <span>выполнено</span> </label>
+                <label class="mr12"> <input type="radio" name="card-state"> <span>невыполнено</span> </label>
+            </div>
+        </div>
+
+        <Card
+            v-for="(arrValues, category) in cards"
+            :key="category" :notes="arrValues"
+            :caption="category"
+        ></Card>
+
+        <router-view name="fogOver"></router-view>
+        <router-view name="editCard"></router-view>
+    </div>
+    <!-- class="container" -->
+</template>
+
+<script>
+import Card from "./components/card.vue";
+import EditCard from "./components/editCard.vue";
+import FogOver from "./components/fogOver.vue";
+
+export default {
+    computed: {
+        cards() {
+            return this.$store.state.cards;
+        },
+    },
+
+    components: {
+        Card,
+        EditCard,
+        FogOver,
+    },
+};
+</script>
